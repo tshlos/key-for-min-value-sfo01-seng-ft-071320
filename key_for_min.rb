9832213ad78e73bbe02c4b48e426ab700188ac02
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-19 02:49:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
     
     smallest = nil
-    smallest_key = ""
+    smallest_key = nil
     
     name_hash.each do |key, value|
         if !smallest || value < smallest
